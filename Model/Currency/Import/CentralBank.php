13667139bb5d253f4c967d8d2d76494d7b79c9c1
Mehmet Akif TEZCAN <akif.tezcan@aurorabilisim.com>
2022-01-18 13:21:37 +0300

    first commit

---
+++ Model/Currency/Import/CentralBank.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Aurora\CentralBankIntegration\Model\Currency\Import;
+
+use Aurora\CentralBankIntegration\Service\Currency;
+use Magento\Framework\HTTP\Client\Curl;
+use Magento\Store\Model\ScopeInterface;
+
+class CentralBank extends \Magento\Directory\Model\Currency\Import\AbstractImport
+{
+    private $scopeConfig;
+    public $currency;
+    public $curl;
+
+
+    public function __construct(
+        \Magento\Directory\Model\CurrencyFactory           $currencyFactory,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        Currency                                           $currency,
+        Curl                                               $curl
+    )
+    {
+        parent::__construct($currencyFactory);
+        $this->scopeConfig = $scopeConfig;
+        $this->currency = $currency;
+        $this->curl = $curl;
+    }
+
+    /**
+     * @param string $currencyFrom
+     * @param string $currencyTo
+     * @param int $retry
+     * @return float|null
+     */
+    protected function _convert($currencyFrom, $currencyTo, $retry = 0)
+    {
+        $result = null;
+        $timeout = (int)$this->scopeConfig->getValue(
+            'currency/currencyLayer/timeout',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
+        $centralBankCurrencyCode = $this->currency->getCentralBankCurrencyCde($currencyTo);
+
+        $url = $this->currency->getUrl($centralBankCurrencyCode);
+
+        $centralBankCurrencyCode = str_replace('.', "_", $centralBankCurrencyCode);
+
+        try {
+            $this->curl->setTimeout($timeout);
+            $this->curl->get($url);
+            $response = json_decode($this->curl->getBody(), true);
+
+            if (isset($response['items'][0][$centralBankCurrencyCode])) {
+                $result = (float)$response['items'][0][$centralBankCurrencyCode];
+            } else {
+                $this->_messages[] = __('We can\'t retrieve a rate from %1.', $url);
+                $this->_convert($currencyFrom, $currencyTo, 1);
+            }
+        } catch (\Exception $e) {
+            if ($retry == 0) {
+                $this->_convert($currencyFrom, $currencyTo, 1);
+            } else {
+                $this->_messages[] = __('We can\'t retrieve a rate from %1.', $url);
+            }
+        }
+        return $result;
+    }
+}
+
+
---
+++ Service/Currency.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Aurora\CentralBankIntegration\Service;
+
+class Currency
+{
+    public $scopeConfig;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    )
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function getCentralBankCurrencyCde($currency)
+    {
+        return "TP.DK." . $currency . ".S";
+    }
+
+    public function getUrl($code)
+    {
+//        $date = date_create("2013-03-15");
+//        echo date_format($date,"l");
+        $key = $this->scopeConfig->getValue(
+            'currency/centralbank/key',
+            \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE
+        );
+
+        $url = $this->scopeConfig->getValue(
+            'currency/centralbank/url',
+            \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE
+        );
+
+        $date = date('d-m-Y');
+        return $url . "series=" . $code . "&key=" . $key . "&startDate=" . $date . "&endDate=" . $date . "&type=json";
+    }
+
+}
